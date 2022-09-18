4bb2fc7298a439be51846bf658919ead881f1537
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-09-18 13:01:51 +0530

    update: es module CDN import

--- basics/js/three.js
+++ basics/js/three.js
@@ -1,3 +1,5 @@
+import * as THREE from "three";
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -7,7 +9,7 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
