cacaf84d0a2367a4b74ad2972987ac35eb9933e4
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-09-19 15:30:01 +0530

    update: render a cube in 3js :vid3

--- basics/js/three.js
+++ basics/js/three.js
@@ -1,22 +1,30 @@
 import * as THREE from "three";
 
+// Create a scene
 const scene = new THREE.Scene();
+
+// Create a camera
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
+camera.position.z = 5;
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+scene.add(camera);
 
-camera.position.z = 5;
 
-const renderer = new THREE.WebGLRenderer();
+// create the canvas to render cube on screen
+const canvas = document.querySelector("canvas.webgl");
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-console.log(THREE);
+// render the cube
+renderer.render(scene, camera);
 // console.log("hello world");
