61ab58249c6250cd2237e9ae7dc98f07cb59232a
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-11-13 07:30:13 +0530

    update: gsap animations

--- 04-webpack/src/script.js
+++ 04-webpack/src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three';
+import gsap from 'gsap';
 
 // scene
 const scene = new THREE.Scene();
@@ -19,12 +20,24 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+// clock
+// constclock = new THREE.Clock();
+
+// animation
+gsap.to(cube.rotation, {rotation:27, x: 2, y: 2, z: 2, duration: 2, repeat: -1, ease: "strong.inOut"});
+
 // animate object
 function animate() {
+
+    // clock
+    // const elapsedTime = clock.getElapsedTime();
+
+
     requestAnimationFrame( animate );
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // camera.rotation.x += 0.01;
+    // camera.rotation.y -= 0.01;
+    // camera.lookAt(cube.position);
 
     renderer.render( scene, camera );
 };
