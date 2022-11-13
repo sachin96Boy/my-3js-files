1780b89cd10bb5cf5e01ebe98c00ffd1f61d9cce
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-11-13 11:31:44 +0530

    update: orbit controllers

--- 04-webpack/src/script.js
+++ 04-webpack/src/script.js
@@ -1,6 +1,41 @@
 import './style.css'
 import * as THREE from 'three';
-import gsap from 'gsap';
+// import gsap from 'gsap';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+let mouse = {
+    x: 0,
+    y: 0
+}
+
+let sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+// mouse
+window.addEventListener('mousemove', (event) => {
+    mouse.x = (event.clientX / window.innerWidth - 0.5);
+    mouse.y = -(event.clientY / window.innerHeight - 0.5);
+})
+
+// resize
+window.addEventListener('resize', () => {
+    // update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    // update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+    renderer.domElement.requestFullscreen();
+    
+});
 
 // scene
 const scene = new THREE.Scene();
@@ -15,6 +50,7 @@ scene.add( cube );
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.z = 5;
 
+
 // render objecrt
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -23,8 +59,9 @@ document.body.appendChild( renderer.domElement );
 // clock
 // constclock = new THREE.Clock();
 
+const controls = new OrbitControls(camera, renderer.domElement);
 // animation
-gsap.to(cube.rotation, {rotation:27, x: 2, y: 2, z: 2, duration: 2, repeat: -1, ease: "strong.inOut"});
+// gsap.to(cube.rotation, {rotation:27, x: 2, y: 2, z: 2, duration: 2, repeat: -1, ease: "strong.inOut"});
 
 // animate object
 function animate() {
@@ -34,6 +71,11 @@ function animate() {
 
 
     requestAnimationFrame( animate );
+
+    // camera.position.x = Math.sin(mouse.x * Math.PI * 2) * 3;
+    // camera.position.z = Math.cos(mouse.x * Math.PI * 2) * 3;
+    // camera.position.y = mouse.y * 2;
+    // camera.lookAt(cube.position);
 
     // camera.rotation.x += 0.01;
     // camera.rotation.y -= 0.01;
